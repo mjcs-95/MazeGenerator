2d01e51bc6a1c792e4457dcd75694ef443b09ba6
Manuel J. Corbacho <m.j.corbacho@hotmail.com>
2019-11-02 05:57:41 +0100

    Add files via upload

---
+++ Scripts/Edge.cs
@@ -0,0 +1,18 @@
+﻿using System;       //Icomparable
+
+public class Edge<T>: IComparable<Edge<T>>  
+        where T : IComparable<T>   
+{
+    public int orig, dest;
+    public T cost;
+    public Edge(int v, int w, T c) {
+        orig = v;
+        dest = w;
+        cost = c;
+    }
+
+    public int CompareTo(Edge<T> other) {
+        if (other == null) return 1;
+        return cost.CompareTo(other.cost);
+    }
+}
